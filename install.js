b1cbd2ae524b1d63ebe3eeab6998461aa4b04c00
sali1502 <sali1502@student.miun.se>
2024-06-23 21:02:11 +0200

    Delete table i install-script

--- install.js
+++ install.js
@@ -17,6 +17,12 @@ connection.connect((err) => {
 })
 
 // Skapa tabell 
+connection.query("DROP TABLE IF EXISTS workexperiences;",
+    (err, results) => {
+        if(err) throw err;
+        console.log("Table workexperiences deleted")
+    });
+
 connection.query(`CREATE TABLE workexperiences (
     id INT AUTO_INCREMENT PRIMARY KEY,
     compayname VARCHAR(255),
