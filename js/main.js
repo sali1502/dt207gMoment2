a161d46e25864422b73efb60339a10e48e62a5df
sali1502 <sali1502@student.miun.se>
2024-06-25 20:57:08 +0200

    Cors, fetch-anrop, hantering av felmeddelanden

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+"use strict";
+
+getData();
+
+async function getData() {
+    const response = await fetch("http://127.0.0.1:3000/api/workexperiences");
+    const data = await response.json();
+    console.log(data);
+}
--- server.js
+++ server.js
@@ -6,25 +6,66 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
 // Routes
 app.get("/api", (req, res) => {
-    res.json({ message: "Welcome to my API" });
+    res.json({ message: "Välkommen till mitt API" });
 });
 
 app.get("/api/workexperiences", (req, res) => {
-    res.json({ message: "Get workexperiences" });
+    res.json({ message: "Hämta arbetserfarenhet" });
 });
 
 app.post("/api/workexperiences", (req, res) => {
-    res.json({ message: "Workexperience added" });
+    let companyname = req.body.companyname;
+    let jobtitle = req.body.jobtitle;
+    let location = req.body.location;
+    let startdate = req.body.startdate;
+    let enddate = req.body.enddate;
+    let description = req.body.description;
+
+    // Felhantering
+    let errors = {
+        message: "",
+        detail: "",
+        https_response: {
+            
+        }
+    };
+
+    if (!companyname || !jobtitle || !location || !startdate || !enddate || !description) {
+
+        // Felmeddelanden
+        errors.message = "Alla fält behöver vara ifyllda!";
+        errors.detail = "Inkludera företagsnamn, titel, plats, startdatum, slutdatum och beskrivning i JSON-format";
+
+        // Svarskod
+        errors.https_response.message = "Bad request";
+        errors.https_response.code = 400;
+        res.status(400).json(errors);
+        return;
+    }
+
+    let workexperience = {
+        companyname: companyname,
+        jobtitle: jobtitle,
+        location: location,
+        startdate: startdate,
+        enddate: enddate,
+        description: description
+    };
+
+    res.json({ message: "Arbetserfarenhet tillagd", workexperience });
 });
 
 app.put("/api/workexperiences/:id", (req, res) => {
-    res.json({ message: "Workexperience uppdated: " + req.params.id });
+    res.json({ message: "Arbetserfarenhet uppdaterad: " + req.params.id });
 });
 
 app.delete("/api/workexperiences/:id", (req, res) => {
-    res.json({ message: "Workexperience deleted: " + req.params.id });
+    res.json({ message: "Arbetserfarenhet raderad: " + req.params.id });
 });
 
 // Anslut till databasen
@@ -37,11 +78,11 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) {
-        console.log("Connection failed: " + err);
+        console.log("Anslutning felaktig: " + err);
     }
-    console.log("Connected to database");
+    console.log("Ansluten till databas");
 });
 
 app.listen(port, () => {
-    console.log("Server is running on port: " + port);
+    console.log("Server på port: " + port);
 });
